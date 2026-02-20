7752dc2fa14ce2e6bfedc15cd23c31a93bf8cb76
Victor <victor13.09.08ferreira@gmail.com>
2026-02-19 21:36:51 -0300

    Arrumei o codigo da porta
    Agora a porta não treme mais e ele abre normal, talvez só precise aumentar a força

--- Japan Market/Assets/Scripts/AbrirPortas/OpenDoors.cs
+++ Japan Market/Assets/Scripts/AbrirPortas/OpenDoors.cs
@@ -2,202 +2,81 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-[System.Serializable]
-	public class GrabObjectClass
-{
-    public bool m_FreezeRotation;
-    public float m_PickupRange = 3f;
-    public float m_ThrowStrength = 50f;
-    public float m_distance = 3f;
-    public float m_maxDistanceGrab = 4f;
-}
-
-[System.Serializable]
-public class ItemGrabClass
-{
-    public bool m_FreezeRotation;
-    public float m_ItemPickupRange = 2f;
-    public float m_ItemThrow = 45f;
-    public float m_ItemDistance = 1f;
-    public float m_ItemMaxGrab = 2.5f;
-}
-
-[System.Serializable]
-public class DoorGrabClass
-{
-    public float m_DoorPickupRange = 2f;
-    public float m_DoorThrow = 10f;
-    public float m_DoorDistance = 2f;
-    public float m_DoorMaxGrab = 3f;
-}
-
-[System.Serializable]
-public class TagsClass
-{
-    public string m_InteractTag = "Interact";
-    public string m_InteractItemsTag = "InteractItem";
-    public string m_DoorsTag = "Door";
-}
-
 public class OpenDoors : MonoBehaviour
-
 {
-	
-	public GameObject playerCam;
 
-    public string GrabButton = "Grab";
-    public string ThrowButton = "Throw";
-    public string UseButton = "Use";
-    public GrabObjectClass ObjectGrab = new GrabObjectClass();
-    public ItemGrabClass ItemGrab = new ItemGrabClass();
-    public DoorGrabClass DoorGrab = new DoorGrabClass();
-    public TagsClass Tags = new TagsClass();
+    [SerializeField] private Camera playerCam;
 
-    private float PickupRange = 3f;
-    private float ThrowStrength = 50f;
-    private float distance = 3f;
-    private float maxDistanceGrab = 4f;
+ 
+    [SerializeField] private string grabButton = "Grab";
+    [SerializeField] private float pickupRange = 2.5f;
+    [SerializeField] private float holdDistance = 2f;
+    [SerializeField] private float maxDistance = 3f;
+    [SerializeField] private float moveForce = 10f;
 
-    private Ray playerAim;
-    private GameObject objectHeld;
-    private bool isObjectHeld;
-    private bool tryPickupObject;
-
-    void Start()
-    {
-        isObjectHeld = false;
-        tryPickupObject = false;
-        objectHeld = null;
-    }
+    private GameObject doorHeld;
+    private bool isHolding;
 
     void FixedUpdate()
     {
-        if (Input.GetButton(GrabButton))
+        if (Input.GetButton(grabButton))
         {
-            if (!isObjectHeld)
-            {
-                tryPickObject();
-                tryPickupObject = true;
-            }
+            if (!isHolding)
+                TryGrabDoor();
             else
-            {
-                holdObject();
-            }
+                HoldDoor();
         }
-        else if (isObjectHeld)
+        else if (isHolding)
         {
-            DropObject();
-        }
-
-        if (Input.GetButton(ThrowButton) && isObjectHeld)
-        {
-            isObjectHeld = false;
-            objectHeld.GetComponent<Rigidbody>().useGravity = true;
-            ThrowObject();
-        }
-
-        if (Input.GetButton(UseButton))
-        {
-            isObjectHeld = false;
-            tryPickObject();
-            tryPickupObject = false;
-            Use();
+            DropDoor();
         }
     }
 
-    private void tryPickObject()
+    void TryGrabDoor()
     {
-        Ray playerAim = playerCam.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray ray = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
 
-        if (Physics.Raycast(playerAim, out hit, PickupRange))
+        if (Physics.Raycast(ray, out hit, pickupRange))
         {
-            objectHeld = hit.collider.gameObject;
-            if (hit.collider.tag == Tags.m_InteractTag && tryPickupObject)
+            if (hit.collider.CompareTag("Door"))
             {
-                isObjectHeld = true;
-                objectHeld.GetComponent<Rigidbody>().useGravity = false;
-                if (ObjectGrab.m_FreezeRotation)
-                {
-                    objectHeld.GetComponent<Rigidbody>().freezeRotation = true;
-                }
-                if (ObjectGrab.m_FreezeRotation == false)
-                {
-                    objectHeld.GetComponent<Rigidbody>().freezeRotation = false;
-                }
-                /**/
-                PickupRange = ObjectGrab.m_PickupRange;
-                ThrowStrength = ObjectGrab.m_ThrowStrength;
-                distance = ObjectGrab.m_distance;
-                maxDistanceGrab = ObjectGrab.m_maxDistanceGrab;
-            }
-            if (hit.collider.tag == Tags.m_InteractItemsTag && tryPickupObject)
-            {
-                isObjectHeld = true;
-                objectHeld.GetComponent<Rigidbody>().useGravity = true;
-                if (ItemGrab.m_FreezeRotation)
-                {
-                    objectHeld.GetComponent<Rigidbody>().freezeRotation = true;
-                }
-                if (ItemGrab.m_FreezeRotation == false)
-                {
-                    objectHeld.GetComponent<Rigidbody>().freezeRotation = false;
-                }
-                /**/
-                PickupRange = ItemGrab.m_ItemPickupRange;
-                ThrowStrength = ItemGrab.m_ItemThrow;
-                distance = ItemGrab.m_ItemDistance;
-                maxDistanceGrab = ItemGrab.m_ItemMaxGrab;
-            }
-            if (hit.collider.tag == Tags.m_DoorsTag && tryPickupObject)
-            {
-                isObjectHeld = true;
-                objectHeld.GetComponent<Rigidbody>().useGravity = true;
-                objectHeld.GetComponent<Rigidbody>().freezeRotation = false;
-                /**/
-                PickupRange = DoorGrab.m_DoorPickupRange;
-                ThrowStrength = DoorGrab.m_DoorThrow;
-                distance = DoorGrab.m_DoorDistance;
-                maxDistanceGrab = DoorGrab.m_DoorMaxGrab;
+                doorHeld = hit.collider.gameObject;
+
+                Rigidbody rb = doorHeld.GetComponent<Rigidbody>();
+                if (rb == null) return;
+
+                rb.useGravity = true;
+                rb.freezeRotation = false;
+
+                isHolding = true;
             }
         }
     }
 
-    private void holdObject()
+    void HoldDoor()
     {
-        Ray playerAim = playerCam.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (doorHeld == null) return;
 
-        Vector3 nextPos = playerCam.transform.position + playerAim.direction * distance;
-        Vector3 currPos = objectHeld.transform.position;
+        Ray ray = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
-        objectHeld.GetComponent<Rigidbody>().velocity = (nextPos - currPos) * 10;
+        Vector3 targetPos = playerCam.transform.position + ray.direction * holdDistance;
+        Vector3 currentPos = doorHeld.transform.position;
 
-        if (Vector3.Distance(objectHeld.transform.position, playerCam.transform.position) > maxDistanceGrab)
+        Rigidbody rb = doorHeld.GetComponent<Rigidbody>();
+        rb.linearVelocity = (targetPos - currentPos) * moveForce;
+
+        if (Vector3.Distance(currentPos, playerCam.transform.position) > maxDistance)
         {
-            DropObject();
+            DropDoor();
         }
     }
 
-    private void DropObject()
+    void DropDoor()
     {
-        isObjectHeld = false;
-        tryPickupObject = false;
-        objectHeld.GetComponent<Rigidbody>().useGravity = true;
-        objectHeld.GetComponent<Rigidbody>().freezeRotation = false;
-        objectHeld = null;
-    }
+        if (doorHeld == null) return;
 
-    private void ThrowObject()
-    {
-        objectHeld.GetComponent<Rigidbody>().AddForce(playerCam.transform.forward * ThrowStrength);
-        objectHeld.GetComponent<Rigidbody>().freezeRotation = false;
-        objectHeld = null;
-    }
-
-    private void Use()
-    {
-        objectHeld.SendMessage("UseObject", SendMessageOptions.DontRequireReceiver); //Every script attached to the PickupObject that has a UseObject function will be called.
-        objectHeld = null;
+        isHolding = false;
+        doorHeld = null;
     }
 }
-
