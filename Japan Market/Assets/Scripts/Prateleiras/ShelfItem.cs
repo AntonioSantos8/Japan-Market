abf33b74fb42b5de455233b0d385a1e136fb5691
Gustavo-Manzoni <gustavomanzoni2008@gmail.com>
2026-02-23 09:36:58 -0300

    QuaseTerminei as estantes

---
+++ Japan Market/Assets/Scripts/Prateleiras/ShelfItem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ShelfItem : MonoBehaviour
+{
+    Segment segment;
+    int groupIndex;
+    int spaceIndex;
+
+    public void Setup(Segment seg, int gIndex, int sIndex)
+    {
+        segment = seg;
+        groupIndex = gIndex;
+        spaceIndex = sIndex;
+    }
+    public Segment GetSegment() => segment;
+    public void RemoveFromShelf()
+    {
+        if (segment != null)
+            segment.FreeSpace(groupIndex, spaceIndex);
+
+        Destroy(this);
+    }
+}
