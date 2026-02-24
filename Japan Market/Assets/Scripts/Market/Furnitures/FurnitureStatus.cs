1b79d882303b6ef796176beb1ad0bf20ae01de02
Yoshizz08 <afsantos082008@gmail.com>
2026-02-24 11:24:32 -0300

    Try -> try to apply the segment system to furnitures

---
+++ Japan Market/Assets/Scripts/Market/Furnitures/FurnitureStatus.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class FurnitureStatus : MonoBehaviour
+{
+    private FurnitureManager _manager;
+    [SerializeField] private FurnitureType _type;
+    //teste
+    [SerializeField] private TMP_Text textQnt;
+    [SerializeField] private SegmentTypeGroup segment;
+    private void Start()
+    {
+        if (_manager == null) return;
+        _manager = ServiceLocator.Get<FurnitureManager>();
+        _type = _manager.GetCurrentSelected().type;
+    }
+    private void Update()
+    {
+        //teste
+        print(segment.GetNullSpace());
+    }
+}
