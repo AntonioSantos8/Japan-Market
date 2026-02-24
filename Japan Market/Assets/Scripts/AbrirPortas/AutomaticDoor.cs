f227d9f4daebc80e4122868ad231f3945ef00a40
Victor <victor13.09.08ferreira@gmail.com>
2026-02-23 23:22:40 -0300

    Porta  automatica pronta e ajustei a porta normal

---
+++ Japan Market/Assets/Scripts/AbrirPortas/AutomaticDoor.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class AutomaticDoor : MonoBehaviour
+{
+    [SerializeField] Transform doorTransform;
+    [SerializeField] Vector3 closePos;
+    [SerializeField] Vector3 openPos;
+    bool isOpen;
+    [SerializeField] float duration;
+    [SerializeField] float distance;
+    [SerializeField] LayerMask layerMask;
+    Coroutine cor;
+
+    private void FixedUpdate()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distance, layerMask);
+
+        bool prevBool = isOpen; 
+         isOpen = colliders.Length > 0;
+
+        if(isOpen != prevBool)
+        {
+           
+            ToggleDoor();
+        }
+
+    }
+    public void ToggleDoor()
+    {
+        if (cor != null)
+        {
+            StopCoroutine(cor);
+        }
+
+        cor = StartCoroutine(TweenCoroutine(isOpen));
+    }
+    private IEnumerator TweenCoroutine (bool open)
+    {
+        isOpen = open;
+
+        Vector3 starPos = doorTransform.position;   
+        Vector3 targetPos = open ? openPos : closePos;
+
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            doorTransform.position = Vector3.Lerp(starPos, targetPos,t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        doorTransform.position = targetPos;
+
+    }
+   
+
+}
