25dc18a77692e3743b348a150330afebb5acf664
Victor <victor13.09.08ferreira@gmail.com>
2026-03-01 23:17:02 -0300

    Agora quando passa o item mostra o valor e o item na maquina registradora

---
+++ Japan Market/Assets/Scripts/CashRegistrer/CashRegister.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+public class CashRegister : MonoBehaviour
+{
+    [SerializeField] private Collider triggerCol;
+    [SerializeField] private List<AllIThingsData> allItem;
+    [SerializeField] private TextMeshPro nameItemText;
+    [SerializeField] private TextMeshPro priceItemText;
+
+    void OnTriggerEnter(Collider other)
+    {
+      if (other == triggerCol) return;
+
+        Item item = other.GetComponent<Item>();
+
+        if (item != null)
+        {
+            Items type = item.GetItemType();
+
+            
+            foreach (var data in allItem)
+            {
+                if (data.itemType == type)
+                {
+                    ShowItemInfo(data);
+                    break; 
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Item item = other.GetComponent<Item>();
+
+        if (item != null)
+        {
+            ClearItem();
+        }
+    }
+
+    void ShowItemInfo(AllIThingsData data)
+    {
+        nameItemText.text = data.itemName;
+        priceItemText.text = "R$ " + data.itemPrice.ToString("F2");
+    }
+
+    void ClearItem()
+    {
+        nameItemText.text = "";
+        priceItemText.text = "";
+    }
+}
