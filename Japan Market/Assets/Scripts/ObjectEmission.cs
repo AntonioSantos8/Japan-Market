c339e5da0863a1579b062c456904ec6cccc55790
Victor <victor13.09.08ferreira@gmail.com>
2026-03-17 11:38:19 -0300

    Começando a forma de pagamento pelo dinheiro

--- Japan Market/Assets/Scripts/ObjectEmission.cs
+++ Japan Market/Assets/Scripts/ObjectEmission.cs
@@ -11,7 +11,7 @@ public class ObjectEmission : MonoBehaviour
     public void ActiveEmission()
     {
         mat.EnableKeyword("_EMISSION");
-        mat.SetColor("_EmissionColor", Color.white * 0.17f);
+        mat.SetColor("_EmissionColor", Color.white * 0.16f);
     }
     public void DisableEmission()
     {
