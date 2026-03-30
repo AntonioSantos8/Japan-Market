7ca8dd476d8d7ebb4253f6bb3cef89b47cbbf1de
Victor <victor13.09.08ferreira@gmail.com>
2026-03-29 23:50:48 -0300

    Agora voce consegue expandir a loja
    Ainda tenho que fazer melhorias,exemplo voce consegue comprar mais de 1 expansao no mesmo botao,depos de comprar ainda o botao aparece pra paga inves de apos comprar aparecer comprado e entre outras coisas

---
+++ Japan Market/Assets/Scripts/Computer/ExpansionStore.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+public class ExpansionStore : MonoBehaviour
+{
+    [SerializeField] GameObject[] storesExpansion;
+    int currentExpansion = 0;
+    void Start()
+    {
+        UpdateStore();
+    }
+    public void BuyExpansion()
+    {
+        if (currentExpansion + 1 >= storesExpansion.Length)
+        {
+            return;
+        }
+
+        currentExpansion++;
+        UpdateStore();
+    }
+    void NextExpansion()
+    {
+        int next = currentExpansion + 1 >= storesExpansion.Length ? 0 : currentExpansion + 1;
+        ChangeExpansion(next);
+    }
+    void PreviousExpansion()
+    {
+        int prev = currentExpansion - 1 < 0 ? storesExpansion.Length - 1 : currentExpansion - 1;
+        ChangeExpansion(prev);
+    }
+    void ChangeExpansion(int newLevel)
+    {
+        if (newLevel == currentExpansion) return;
+
+
+
+        currentExpansion = newLevel;
+        UpdateStore();
+    }
+    void UpdateStore()
+    {
+        for (int i = 0; i < storesExpansion.Length; i++)
+        {
+            storesExpansion[i].SetActive(i == currentExpansion);
+        }
+    }
+    public int GetCurrentExpansion()
+    {
+        return currentExpansion;
+    }
+}
