ee437b31421def5c2aaf408873794cbb183c93b1
Victor <victor13.09.08ferreira@gmail.com>
2026-05-07 08:46:35 -0300

    Deu agora esta bom a coisa do giving do dinhheiro

--- Japan Market/Assets/Scripts/CashRegistrer/PaymentCard.cs
+++ Japan Market/Assets/Scripts/CashRegistrer/PaymentCard.cs
@@ -86,7 +86,8 @@ public class PaymentCard : MonoBehaviour
         ServiceLocator.Get<MarketManager>().Earn_Money(totalPrice);
         seq.Append(valueText.DOColor(Color.darkGreen, 0.2f));
         seq.AppendInterval(0.2f);
-        seq.Append(imagepayment.transform.DOScale(0f, 0.3f).SetEase(Ease.InOutSine));
+        seq.Append(valueText.DOColor(Color.white, 0.2f));
+        seq.Append(imagepayment.transform.DOScale(0f, 0.26f).SetEase(Ease.InOutSine));
 
         seq.OnComplete(() =>
         {
